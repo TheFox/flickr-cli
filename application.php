4159965455031714915dd6c314538759a7ec263a
Christian Mayer <christian@fox21.at>
2016-12-23 16:21:02 +0100

    Files Command added.
    Closes #5.

--- application.php
+++ application.php
@@ -8,12 +8,14 @@ use Symfony\Component\Console\Application;
 use TheFox\FlickrCli\FlickrCli;
 use TheFox\FlickrCli\Command\AuthCommand;
 use TheFox\FlickrCli\Command\DownloadCommand;
+use TheFox\FlickrCli\Command\FilesCommand;
 use TheFox\FlickrCli\Command\ListCommand;
 use TheFox\FlickrCli\Command\UploadCommand;
 
 $application = new Application(FlickrCli::NAME, FlickrCli::VERSION);
 $application->add(new AuthCommand());
 $application->add(new DownloadCommand());
+$application->add(new FilesCommand());
 $application->add(new ListCommand());
 $application->add(new UploadCommand());
 $application->run();
