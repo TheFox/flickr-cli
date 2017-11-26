a096a3b00cd34115ec026360199a25fedfa66ad8
Christian Mayer <christian@fox21.at>
2017-11-26 12:52:51 +0100

    Version 2.0.0-rc.1.

--- src/TheFox/FlickrCli/FlickrCli.php
+++ src/TheFox/FlickrCli/FlickrCli.php
@@ -5,7 +5,7 @@ namespace TheFox\FlickrCli;
 class FlickrCli
 {
     const NAME = 'FlickrCli';
-    const VERSION = '2.0.0-dev.6';
+    const VERSION = '2.0.0-rc.1';
 
     const UPLOAD_PROGRESSBAR_ITEMS = 35;
     const DOWNLOAD_PROGRESSBAR_ITEMS = 35;
